929b883fead091dfe38dd060e3d37ee579b45074
irvynmccley <mccley.1@gmail.com>
2026-04-16 18:27:28 -0300

    atualização manual
    atualização manual

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Ponto de entrada da aplicação (Entry point)
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
